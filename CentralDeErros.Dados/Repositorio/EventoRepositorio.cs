742c3b40288b9679be1fb14b3d122ccbf29d62a3
guilhermejunkes <guilhermepjunkes@gmail.com>
2020-07-23 20:37:50 -0300

    Conexão com banco e migration

--- CentralDeErros.Dados/Repositorio/EventoRepositorio.cs
+++ CentralDeErros.Dados/Repositorio/EventoRepositorio.cs
@@ -1,6 +1,7 @@
 ﻿using CentralDeErros.Dominio.Modelos;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CentralDeErros.Dados.Repositorio
 {
@@ -20,36 +21,7 @@ namespace CentralDeErros.Dados.Repositorio
         }
         public List<Evento> SelecionarTodos()
         {
-            var evento1 = new Evento()
-            {
-                Id = 1,
-                Ambiente = 1,
-                Level = 1,
-                Titulo = "Teste 001",
-                Quantidade = 1,
-                Data = new DateTime(),
-                Descricao = "Teste 001",
-                Origem = "Teste 001"
-            };
-
-            var evento2 = new Evento()
-            {
-                Id = 2,
-                Ambiente = 2,
-                Level = 2,
-                Titulo = "Teste 002",
-                Quantidade = 2,
-                Data = new DateTime(),
-                Descricao = "Teste 002",
-                Origem = "Teste 002"
-            };
-
-            var eventos = new List<Evento>();
-            eventos.Add(evento1);
-            eventos.Add(evento2);
-
-            return eventos;
-            //return _contexto.Evento.ToList();
+            return _contexto.Evento.ToList();
         }
     }
 }
